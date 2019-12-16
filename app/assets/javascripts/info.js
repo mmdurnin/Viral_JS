1f6af52f26da4fb2bb5c5ca8bdb02f0c24d27d89
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-16 14:29:14 -0800

    add even listener to info button, set up structure for opening info modal

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -1,4 +1,19 @@
-const infoButton = document.getElementById('info-button')
-const infoWindow = document.getElementById('info')
-const infoModal = document.getElementById('info-modal')
+window.addEventListener("DOMContentLoaded", () => {
+    const infoButton = document.getElementById('info-button')
+    const infoWindow = document.getElementById('info')
+    const infoModal = document.getElementById('info-modal')
+    
+    let modalStatus = "closed"
+    
+    const handleModal = () => {
+        if (modalStatus === "closed") {
+            modalStatus = "open";
+            infoButton.innerText = "Close"
+        } else {
+            modalStatus = "closed";
+            infoButton.innerText = "About Viral"
+        }
+    }
 
+    infoButton.addEventListener("click", handleModal)
+})
