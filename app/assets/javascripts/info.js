f32d17edfd53694686b4cfcfbe48b4fba849c27f
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-16 15:15:07 -0800

    fade in effect for modal

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -5,15 +5,24 @@ window.addEventListener("DOMContentLoaded", () => {
     
     let modalStatus = "closed"
     
-    const handleModal = () => {
+    const handleModal = (e) => {
+        if (e.target.id === "info-modal") return
         if (modalStatus === "closed") {
             modalStatus = "open";
             infoButton.innerText = "Close"
+
+            infoWindow.classList = "active-modal-back"
+            infoModal.classList = "active-modal-front"
+            infoModal.innerText = "Welcome to Viral"
         } else {
             modalStatus = "closed";
             infoButton.innerText = "About Viral"
+            infoWindow.classList.remove("active-modal-back")
+            infoModal.classList.remove("active-modal-front")
         }
     }
 
     infoButton.addEventListener("click", handleModal)
+    infoWindow.addEventListener("click", (e) => handleModal(e))
+    infoModal.addEventListener("click", () => {return})
 })
