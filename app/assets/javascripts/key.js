4476b573ce496057b1222db2391fc7ea1749a925
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-17 15:03:50 -0800

    add colors to key. Need to work out sizing and margin

--- app/assets/javascripts/key.js
+++ app/assets/javascripts/key.js
@@ -50,6 +50,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // 'tb', '#fbb03b',
+    // 'chlamydia', '#3bb2d0',
+    // 'Hepatitis  A', '#e55e5e',
+    // 'Acute Viral Hepatitis B', '#223b53'
+
 
     keyButton.addEventListener("click", handleKey)
 })
