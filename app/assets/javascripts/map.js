68d401add472da0ca6324f440a754fc94e74692a
Maureen Durnin <maureenmdurnin@gmail.com>
2022-06-29 13:21:17 -0700

    Paint data in filtered layers rather than one layer that gets refiltered

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,3 +1,35 @@
+// TODO: Create loading state
+let isLoading = true;
+
+const END_RADIUS = 50;
+const RADIUS_STEP = .25;
+const START_OPACITY = 1;
+const END_OPACITY = 0;
+const OPACITY_STEP = .02;
+
+const COLOR_MAP = {
+    tb: ['rgb(232,213,247)', 'rgb(168,144,227)', 'rgb(161,122,228)'],
+    chlamydia: ['rgb(253,194,133)', 'rgb(255,165,121)', 'rgb(249,137,118)'],
+    'Hepatitis  A': ['rgb(210,241,163)', 'rgb(184,229,165)', 'rgb(123,203,162)', 'rgb(70,174,159)'],
+    'Acute Viral Hepatitis B': ['rgb(31,122,121)', 'rgb(14,88,97)', 'rgb(7,64,81)'],
+    COVID: ['#ccc'],
+
+}
+const RADIUS_MAP = {
+    tb: [1,2,3],
+    chlamydia: [2,3,4],
+    'Hepatitis  A': [1,2,3],
+    'Acute Viral Hepatitis B': [1,2,3],
+    COVID: [1,2,3],
+}
+
+function getColor(diseaseName) {
+    return COLOR_MAP[diseaseName][Math.floor(Math.random() * COLOR_MAP[diseaseName].length)]
+}
+function getRadius(diseaseName) {
+    return RADIUS_MAP[diseaseName][Math.floor(Math.random() * RADIUS_MAP[diseaseName].length)]
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     mapboxgl.accessToken = 'pk.eyJ1IjoibWR1cm5pbiIsImEiOiJjazNjaHkxZjEweXg3M2lvMWtnZDh0M3g5In0.ND3SU3mXL1jlXr5EEWMyug';
 
@@ -13,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 {
                     id: 'background',
                     type: 'background',
-                    paint: { 'background-color': 'rgb(173,193,217)' }
+                    paint: { 'background-color': 'rgb(10,0,38)' }
                 }
             ]
 
@@ -38,8 +70,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Draws the US map onto the page
 function decorateMap(states, map) {
-    
-    var hoveredStateId = null;
+    if (isLoading) {
+        document.getElementById("nav-button").style.cursor = "wait";
+        document.getElementById("nav-button").disabled = true;
+        console.log('Loading...')
+    }
+
+    let hoveredStateId = null;
 
     const json = JSON.parse(JSON.stringify(states))
 
@@ -56,7 +93,7 @@ function decorateMap(states, map) {
         "source": "states",
         "layout": {},
         "paint": {
-            "fill-color": "rgb(247,246,247)",
+            "fill-color": "rgb(241,230,222)",
             "fill-opacity": ["case",
                 ["boolean", ["feature-state", "hover"], false],
                 .75,
@@ -71,8 +108,9 @@ function decorateMap(states, map) {
         "source": "states",
         "layout": {},
         "paint": {
-            "line-color": "white",
-            "line-width": 1
+            "line-color": "rgb(101,114,146)",
+            "line-width": 1,
+            "line-dasharray": [1, 1],
         }
     });
 
@@ -104,8 +142,12 @@ function decorateMap(states, map) {
     // Zoom into state on click (this does not interfere with map dragging)
     map.on("click", "state-fills", function (e) {
         if (e.features.length > 0) {
+            // Index into coordinates, NOTE: Some states have an additional layer of coordinate nesting
             let coordsArray = e.features[0].geometry.coordinates[0];
             coordsArray = coordsArray.length === 1 ? coordsArray[0] : coordsArray;
+            // Determine the middle of the clicked state 
+            // Note: coords array always starts and ends top left. To get bottom right, 
+            // we derive the coordinates from the middle of the coords array
             const topLeftCoord = coordsArray[0];
             const bottomRightCoord = coordsArray[Math.floor(coordsArray.length / 2)];
             const xCoord = (topLeftCoord[0] + bottomRightCoord[0]) / 2;
@@ -119,115 +161,107 @@ function decorateMap(states, map) {
 
 // Prepares disease data that will populate the map when filters are set
 function populateMap(pops, map) {
+    isLoading = false;
+    console.log("Finished loading")
+    document.getElementById("nav-button").style.cursor = "pointer";
+    document.getElementById('nav-button').disabled = false;
 
     const json = JSON.parse(JSON.stringify(pops))
+    const ticker = document.getElementById('slider');
+    let selectedDisease = null;
 
     map.addSource("disease_pops", {
         "type": "geojson",
         "data": json
     });
 
-    map.addLayer({
-        "id": "pop-points",
-        "type": "circle",
-        "source": "disease_pops",
-        "layout": {},
-        "paint": {
-            'circle-opacity': 0.2,
-            'circle-opacity-transition': {duration: 800},
-            'circle-color': [
-            'match',
-                ['get', 'name'],
-                'tb', '#fbb03b',
-                'chlamydia', '#3bb2d0',
-                'Hepatitis  A', '#e55e5e',
-                'Acute Viral Hepatitis B', '#223b53',
-                '#ccc'
-            ],
-            "circle-radius": [
-                'match',
-                    ['get', 'name'],
-                    'tb', 2,
-                    'chlamydia', 3,
-                    'Hepatitis  A', 2,
-                    'Acute Viral Hepatitis B', 2,
-                    2
-            ],
-            "circle-radius-transition": {duration: 800},
+    function fadeCircles(layerId, startingRadius) {
+        let currentRadius = startingRadius;
+        let currentOpacity = START_OPACITY;
+        function expandRadius() {
+            currentRadius+= RADIUS_STEP;
+            currentOpacity-= OPACITY_STEP;
+            if (map.getLayer(layerId) && currentOpacity > 0) {
+                map.setPaintProperty(layerId, 'circle-radius', currentRadius);
+                map.setPaintProperty(layerId, 'circle-opacity', currentOpacity);
+            }
+            if (currentRadius < END_RADIUS && currentOpacity > 0) {
+                window.requestAnimationFrame(expandRadius)
+            }
         }
-    })
+        const animation = window.requestAnimationFrame(expandRadius)
 
+        setTimeout(function() {
+            if (map.getLayer(layerId)) {
+                map.removeLayer(layerId)
+                window.cancelAnimationFrame(animation)
+            }
+        }, 2000)
+    }
+
+    function addFilteredLayer(year) {
+        const id = `pop-points-${year}`
+        const radius = getRadius(selectedDisease)
+
+        if (selectedDisease === null) return
+
+        map.addLayer({
+            id,
+            type: "circle",
+            source: "disease_pops",
+            layout: {},
+            filter: [
+                'all',
+                ['==', 'year', year],
+                ['==', 'name', selectedDisease],
+            ],
+            paint: {
+                'circle-opacity': 1,
+                'circle-color': getColor(selectedDisease),
+                "circle-radius": radius,
+            }
+        })
+
+        fadeCircles(id, radius)
+    }
+
+    // Advance ticker on an automatic timelapse
+    function playTimeLapse() {
+        let tickerPop = 2000; 
+        const keepSpreading = setInterval(function () {
+            tickerPop += 1
+            addFilteredLayer(tickerPop);
+            ticker.stepUp();
+            if (tickerPop >= 2017) {
+                clearInterval(keepSpreading)
+                ticker.value = 2000;
+            }
+        }, 500);
+    }
+
+    // Handle checking filters off/on
     document.getElementById('disease-form').addEventListener("change", function (e) {
         if (e.target.checked === true) {
+            selectedDisease = e.target.dataset.name.split("_").join(" ")
             e.target.value = true
         } else {
             e.target.value = false
         }
     });
 
-    let diseaseDisplay = [];
+    // Handle filter submission
     document.getElementById('submit-filters').addEventListener('click', function (e) {
-        diseaseDisplay = [];
         e.preventDefault();
-        const diseaseFilters = document.getElementById('disease-form').elements
 
-        for (let i = 0; i < diseaseFilters.length; i++) {
-            currentDisease = diseaseFilters[i]
-            if (currentDisease.value === "true") {
-                dName = currentDisease.name.split("_").join(" ")
-                diseaseDisplay.push(dName)
-            }
+        if (selectedDisease) {
+            playTimeLapse(); 
         }
-        playTimeLapse();
     })
 
-    function filterBy(year, diseaseArr) {
-        diseaseArr.unshift('name')
-        diseaseArr.unshift('in')
-
-        var filters = ['all', ['==', 'year', year], diseaseArr]
-        map.setFilter('pop-points', filters)
-        diseaseArr.shift()
-        diseaseArr.shift()
-        diseaseArr = [];
-    }
-
-    const ticker = document.getElementById('slider');
-
-    function clearMap(){
-        ticker.value = 2000;
-        filterBy(1990, []);
-    }
-
-
+    // Handle cases where ticker is manually moved
     ticker.addEventListener('input', function (e) {
         var year = parseInt(e.target.value, 10);
-        filterBy(year, diseaseDisplay);
+        addFilteredLayer(year);
     });
-    clearMap();
-
-    function fadeCircles() {
-        setTimeout(function() {
-            map.setPaintProperty('pop-points', 'circle-opacity', 1);
-        }, 100);
-        setTimeout(function () {
-            map.setPaintProperty('pop-points', 'circle-opacity', .6);
-        }, 500);
-    }
-
-    
-    function playTimeLapse() {
-        let ticker_pop = 2000; 
-        const keepSpreading = setInterval(function () {
-            ticker_pop += 1
-            fadeCircles();
-            filterBy(ticker_pop, diseaseDisplay);
-            ticker.stepUp();
-            if (ticker_pop >= 2017) {
-                clearInterval(keepSpreading)
-                clearMap();
-            }
-        }, 500);
-    }
 }
 
